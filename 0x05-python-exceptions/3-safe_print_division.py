b8a69e1823f49326503cacc4b947164a5c5e5467
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-10-23 03:03:19 -0700

    0x05 try except

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    out = 0
+    try:
+        out = a/b
+    except ZeroDivisionError:
+        out = None
+    finally:
+        print("Inside result: {}".format(out))
+    return out
