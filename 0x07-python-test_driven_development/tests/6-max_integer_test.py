51d7b3bc3911bc68f1599218f717e60b43986aac
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-10-28 05:47:47 -0700

    unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInt(unittest.TestCase):
+    def test_max(self):
+        array = [
+                [5, 6 ,20],
+                [100, 6, 10],
+                []
+                ]
+        result = [max_integer(i) for i in array]
+        self.assertEqual(result, [20, 100, None])
+
+    def test_None(self):
+        result = max_integer([])
+        self.assertEqual(result, None)
+
+if "__name__" == "__main__":
+    unittest.main()
