a80eb2fd395944c170bd3a9cc1fafa7867b6ff3d
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2023-10-30 02:22:07 -0700

    more python classes 2

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+rectangle module
+rectangle class
+"""
+
+
+class Rectangle:
+    """ Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """ init function
+        Args:
+        width (int): the width
+        height (int): the height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """property width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """prop height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """set the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """set the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
