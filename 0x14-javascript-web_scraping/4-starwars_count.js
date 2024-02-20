9b7f80d23276980f4f163facae4d7d068d6894b4
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-02-20 11:41:35 +0100

    IDK

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
+request(process.argv[2], function (error, _, body) {
   if (error) {
     console.log(error);
   } else {
-    const pattern = /\/people\/18\//g;
-    const matches = body.match(pattern);
-    console.log(matches.length);
+    const json = JSON.parse(body);
+    let count = 0;
+    for (const film of json.results) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
